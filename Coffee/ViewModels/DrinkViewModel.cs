c3fb05323f8d6617b3bcfdd4e594664cafd04818
yoannakr <jkrmilkova@gmail.com>
2020-07-29 15:15:12 +0300

    add selectedItem option

--- Coffee/ViewModels/DrinkViewModel.cs
+++ Coffee/ViewModels/DrinkViewModel.cs
@@ -1,17 +1,42 @@
-﻿using Coffee.Models;
+﻿using Prism.Commands;
+using Coffee.Models;
+using System.Windows.Input;
+using Coffee.DataSettings;
 using Coffee.DataSettings.Implementations;
 using System.Collections.ObjectModel;
 using static Coffee.DataSettings.DataSetting;
-using Coffee.DataSettings;
+using System;
 
 namespace Coffee.ViewModels
 {
     public class DrinkViewModel : BaseViewModel
     {
-        private static readonly IReader xml = new XmlReader();
+        private ICommand addDrinkCommand;
+        private readonly IReader xml = new XmlReader();
 
-        public ObservableCollection<Drink> Drinks { get; } = xml.ReadData<Drink>(DrinksXml);
+        public int Count { get; set; }
 
+        public string DrinkName { get; set; }
 
+        public ObservableCollection<Drink> Drinks
+        {
+            get => xml.ReadData<Drink>(DrinksXml);
+        }
+
+        public ICommand AddDrinkCommand
+        {
+            get
+            {
+                if (addDrinkCommand == null)
+                    addDrinkCommand = new DelegateCommand<object>(AddDrinkToSelectedItem);
+
+                return addDrinkCommand;
+            }
+        }
+
+        private void AddDrinkToSelectedItem(object obj)
+        {
+
+        }
     }
 }
